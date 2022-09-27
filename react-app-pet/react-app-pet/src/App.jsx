3250b0386848e1856fdd59483a5ec57ff982df20
EmersonGald1n0 <rm93899@fiap.com.br>
2022-09-27 01:00:33 -0300

    Base

---
+++ react-app-pet/react-app-pet/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import PetCadastro from './components/petCadastro/PetCadastro'
+
+export default function App() {
+  return (
+    <div>
+      <h1>Estudo com Styled-Components</h1>
+      <PetCadastro />
+    </div>
+  );
+}
